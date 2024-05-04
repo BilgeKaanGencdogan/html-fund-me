0f4008bb93532996cd3ffcbc16b5ee43135a2dc6
Bilge Kaan Gençdoğan <kaan.gencdogan@ug.bilkent.edu.tr>
2024-05-04 23:09:27 +0300

    error: Uncaught (in promise) Error: missing provider (argument="provider", value=undefined, code=INVALID_ARGUMENT, version=providers/5.1.2)

---
+++ index.js
@@ -0,0 +1,40 @@
+import { abi, contractAddress } from "./constans.js"
+import { ethers } from "./ethers-5.1.esm.min.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+        connectButton.innerHTML = "Connected!"
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+    } else {
+        connectButton.innerHTML = "Please install MetaMask!!"
+    }
+}
+
+async function fund() {
+    const ethAmount = "77"
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        // provider / connection to the blockchain
+        // signer / wallet / someoen with some gas
+        // contract that we are interacting with
+        // ^ ABI & address
+        const provider = new ethers.providers.Web3Provider(Window.ethereum)
+        const signer = provider.getSigner()
+        console.log(signer)
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+
+        const transactionResponse = await contract.fund({
+            value: ethers.parseEther(ethAmount),
+        })
+    }
+}
